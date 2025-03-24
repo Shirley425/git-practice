6a7dc80fd9775dcc51f7ae3639fb9261216524c6
Gitika Kalal <gitikakalal21@gmail.com>
2025-03-24 14:43:54 -0700

    resolve merge conflict

--- function_d.py
+++ function_d.py
@@ -2,12 +2,9 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-<<<<<<< HEAD
     #Gitika
     pass
-=======
 #Qiaoqiao
->>>>>>> 9766e6c2bfa0cacf371f5080cd7755a501ec8da6
 
 
 if __name__ == "__main__":
